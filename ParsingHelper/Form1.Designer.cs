3997d435c503238f972e655342b0dc121eb3f28f
ratkosrb <ratkomladic2@abv.bg>
2022-01-28 19:35:33 +0200

    Add Project.

---
+++ ParsingHelper/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace ParsingHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtWppDirectory = new System.Windows.Forms.TextBox();
+            this.lblWppDirectory = new System.Windows.Forms.Label();
+            this.btnBrowseWpp = new System.Windows.Forms.Button();
+            this.btnBrowseSniffs = new System.Windows.Forms.Button();
+            this.lblSniffsDirectory = new System.Windows.Forms.Label();
+            this.txtSniffsDirectory = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.lblMode = new System.Windows.Forms.Label();
+            this.cmbMode = new System.Windows.Forms.ComboBox();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.clmVersion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnFileCount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnReverseSelection = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtWppDirectory
+            // 
+            this.txtWppDirectory.Location = new System.Drawing.Point(12, 29);
+            this.txtWppDirectory.Name = "txtWppDirectory";
+            this.txtWppDirectory.Size = new System.Drawing.Size(376, 20);
+            this.txtWppDirectory.TabIndex = 0;
+            // 
+            // lblWppDirectory
+            // 
+            this.lblWppDirectory.AutoSize = true;
+            this.lblWppDirectory.Location = new System.Drawing.Point(12, 9);
+            this.lblWppDirectory.Name = "lblWppDirectory";
+            this.lblWppDirectory.Size = new System.Drawing.Size(80, 13);
+            this.lblWppDirectory.TabIndex = 1;
+            this.lblWppDirectory.Text = "WPP Directory:";
+            // 
+            // btnBrowseWpp
+            // 
+            this.btnBrowseWpp.Location = new System.Drawing.Point(394, 26);
+            this.btnBrowseWpp.Name = "btnBrowseWpp";
+            this.btnBrowseWpp.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseWpp.TabIndex = 2;
+            this.btnBrowseWpp.Text = "Browse";
+            this.btnBrowseWpp.UseVisualStyleBackColor = true;
+            this.btnBrowseWpp.Click += new System.EventHandler(this.btnBrowseWpp_Click);
+            // 
+            // btnBrowseSniffs
+            // 
+            this.btnBrowseSniffs.Location = new System.Drawing.Point(394, 72);
+            this.btnBrowseSniffs.Name = "btnBrowseSniffs";
+            this.btnBrowseSniffs.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseSniffs.TabIndex = 5;
+            this.btnBrowseSniffs.Text = "Browse";
+            this.btnBrowseSniffs.UseVisualStyleBackColor = true;
+            this.btnBrowseSniffs.Click += new System.EventHandler(this.btnBrowseSniffs_Click);
+            // 
+            // lblSniffsDirectory
+            // 
+            this.lblSniffsDirectory.AutoSize = true;
+            this.lblSniffsDirectory.Location = new System.Drawing.Point(12, 55);
+            this.lblSniffsDirectory.Name = "lblSniffsDirectory";
+            this.lblSniffsDirectory.Size = new System.Drawing.Size(81, 13);
+            this.lblSniffsDirectory.TabIndex = 4;
+            this.lblSniffsDirectory.Text = "Sniffs Directory:";
+            // 
+            // txtSniffsDirectory
+            // 
+            this.txtSniffsDirectory.Location = new System.Drawing.Point(12, 75);
+            this.txtSniffsDirectory.Name = "txtSniffsDirectory";
+            this.txtSniffsDirectory.Size = new System.Drawing.Size(376, 20);
+            this.txtSniffsDirectory.TabIndex = 3;
+            this.txtSniffsDirectory.Leave += new System.EventHandler(this.txtSniffsDirectory_Leave);
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmVersion,
+            this.columnFileCount});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 101);
+            this.listView1.Name = "listView1";
+            this.listView1.ShowItemToolTips = true;
+            this.listView1.Size = new System.Drawing.Size(458, 259);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // lblMode
+            // 
+            this.lblMode.AutoSize = true;
+            this.lblMode.Location = new System.Drawing.Point(14, 370);
+            this.lblMode.Name = "lblMode";
+            this.lblMode.Size = new System.Drawing.Size(37, 13);
+            this.lblMode.TabIndex = 7;
+            this.lblMode.Text = "Mode:";
+            // 
+            // cmbMode
+            // 
+            this.cmbMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMode.FormattingEnabled = true;
+            this.cmbMode.Items.AddRange(new object[] {
+            "Individual",
+            "Mass"});
+            this.cmbMode.Location = new System.Drawing.Point(57, 367);
+            this.cmbMode.Name = "cmbMode";
+            this.cmbMode.Size = new System.Drawing.Size(90, 21);
+            this.cmbMode.TabIndex = 8;
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(395, 365);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(75, 23);
+            this.btnParse.TabIndex = 9;
+            this.btnParse.Text = "Parse";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // clmVersion
+            // 
+            this.clmVersion.Text = "Version";
+            this.clmVersion.Width = 300;
+            // 
+            // columnFileCount
+            // 
+            this.columnFileCount.Text = "File Count";
+            this.columnFileCount.Width = 130;
+            // 
+            // btnReverseSelection
+            // 
+            this.btnReverseSelection.Location = new System.Drawing.Point(282, 365);
+            this.btnReverseSelection.Name = "btnReverseSelection";
+            this.btnReverseSelection.Size = new System.Drawing.Size(107, 23);
+            this.btnReverseSelection.TabIndex = 10;
+            this.btnReverseSelection.Text = "Reverse Selection";
+            this.btnReverseSelection.UseVisualStyleBackColor = true;
+            this.btnReverseSelection.Click += new System.EventHandler(this.btnReverseSelection_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(197, 367);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(79, 20);
+            this.txtName.TabIndex = 11;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(153, 370);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 12;
+            this.lblName.Text = "Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 393);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnReverseSelection);
+            this.Controls.Add(this.btnParse);
+            this.Controls.Add(this.cmbMode);
+            this.Controls.Add(this.lblMode);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnBrowseSniffs);
+            this.Controls.Add(this.lblSniffsDirectory);
+            this.Controls.Add(this.txtSniffsDirectory);
+            this.Controls.Add(this.btnBrowseWpp);
+            this.Controls.Add(this.lblWppDirectory);
+            this.Controls.Add(this.txtWppDirectory);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Parsing Helper for Handicapped People";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtWppDirectory;
+        private System.Windows.Forms.Label lblWppDirectory;
+        private System.Windows.Forms.Button btnBrowseWpp;
+        private System.Windows.Forms.Button btnBrowseSniffs;
+        private System.Windows.Forms.Label lblSniffsDirectory;
+        private System.Windows.Forms.TextBox txtSniffsDirectory;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label lblMode;
+        private System.Windows.Forms.ComboBox cmbMode;
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.ColumnHeader clmVersion;
+        private System.Windows.Forms.ColumnHeader columnFileCount;
+        private System.Windows.Forms.Button btnReverseSelection;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
---
+++ ParsingHelper/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ParsingHelper
+{
+    public partial class Form1 : Form
+    {
+        enum PktVersion
+        {
+            NoHeader = 0,
+            V2_1 = 0x201,
+            V2_2 = 0x202,
+            V3_0 = 0x300,
+            V3_1 = 0x301
+        }
+
+        const string MASS_PARSE_VERSION_PH = "@PHTOKENVERSION@";
+        const string MASS_PARSE_NAME_PH = "@PHTOKENNAME@";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseWpp_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            folderBrowserDialog1.SelectedPath = txtWppDirectory.Text;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtWppDirectory.Text = folderBrowserDialog1.SelectedPath;
+                if (!System.IO.File.Exists(Path.Combine(txtWppDirectory.Text, "WowPacketParser.exe")))
+                    MessageBox.Show("WowPacketParser.exe not found in directory!", "Parsing Helper", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        Dictionary<uint, List<string>> _sniffsPerBuild = new Dictionary<uint, List<string>>();
+        private void AddSniffToList(uint build, string sniff)
+        {
+            if (build == 0)
+            {
+                MessageBox.Show("Skipping file with unknown build:\r\n" + sniff);
+                return;
+            }
+
+            if (_sniffsPerBuild.ContainsKey(build))
+            {
+                _sniffsPerBuild[build].Add(sniff);
+            }
+            else
+            {
+                List<string> fileList = new List<string>();
+                fileList.Add(sniff);
+                _sniffsPerBuild.Add(build, fileList);
+            }
+        }
+        private void ShowSniffsInListView()
+        {
+            listView1.Items.Clear();
+            listView1.Sorting = SortOrder.None;
+            foreach (var itr in _sniffsPerBuild)
+            {
+                string versionName;
+                if (Enum.IsDefined(typeof(ClientVersionBuild), (int)itr.Key))
+                    versionName = ((ClientVersionBuild)itr.Key).ToString();
+                else
+                    versionName = "Build_" + itr.Key.ToString();
+
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = versionName;
+                lvi.SubItems.Add(itr.Value.Count.ToString());
+                lvi.Tag = itr.Value;
+                
+                foreach (var fileName in itr.Value)
+                {
+                    if (!String.IsNullOrEmpty(lvi.ToolTipText))
+                        lvi.ToolTipText += "\r\n";
+                    lvi.ToolTipText += Path.GetFileName(fileName);
+                }
+
+                listView1.Items.Add(lvi);
+            }
+            listView1.Sorting = SortOrder.Ascending;
+            listView1.Sort();
+        }
+        private void LoadAllSniffsInDirectory()
+        {
+            _sniffsPerBuild.Clear();
+            foreach (string file in Directory.EnumerateFiles(txtSniffsDirectory.Text, "*.pkt", SearchOption.AllDirectories))
+            {
+                AddSniffToList(ReadSniffHeader(file), file);
+            }
+            ShowSniffsInListView();
+        }
+        private void txtSniffsDirectory_Leave(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtSniffsDirectory.Text))
+                LoadAllSniffsInDirectory();
+        }
+        private void btnBrowseSniffs_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            folderBrowserDialog1.SelectedPath = txtSniffsDirectory.Text;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtSniffsDirectory.Text = folderBrowserDialog1.SelectedPath;
+                LoadAllSniffsInDirectory();
+            }
+        }
+
+        private uint ReadSniffHeader(string filePath)
+        {
+            PktVersion pktVersion = PktVersion.NoHeader;
+            uint build = 0;
+            uint snifferId = 0;
+            int snifferVersion = 0;
+            long startTime = 0;
+            uint startTickCount = 0;
+            string locale = "";
+
+            using (FileStream fs = File.OpenRead(filePath))
+            using (BinaryReader reader = new BinaryReader(fs))
+            {
+                var headerStart = reader.ReadBytes(3);             // PKT
+                if (Encoding.ASCII.GetString(headerStart) != "PKT")
+                {
+                    // file does not have a header
+                    reader.BaseStream.Position = 0;
+                    return 0;
+                }
+
+                pktVersion = (PktVersion)reader.ReadUInt16();      // sniff version
+
+                int additionalLength;
+
+                switch (pktVersion)
+                {
+                    case PktVersion.V2_1:
+                    {
+                        build = reader.ReadUInt16(); // client build
+                        reader.ReadBytes(40);        // session key
+                        break;
+                    }
+                    case PktVersion.V2_2:
+                    {
+                        snifferId = reader.ReadByte();            // sniffer id
+                        build = reader.ReadUInt16();              // client build
+                        reader.ReadBytes(4);                      // client locale
+                        reader.ReadBytes(20);                     // packet key
+                        reader.ReadBytes(64);                     // realm name
+                        break;
+                    }
+                    case PktVersion.V3_0:
+                    {
+                        snifferId = reader.ReadByte();            // sniffer id
+                        build = reader.ReadUInt32();              // client build
+                        reader.ReadBytes(4);                      // client locale
+                        reader.ReadBytes(40);                     // session key
+                        additionalLength = reader.ReadInt32();
+                        var preAdditionalPos = reader.BaseStream.Position;
+                        reader.ReadBytes(additionalLength);
+                        var postAdditionalPos = reader.BaseStream.Position;
+                        if (snifferId == 10)                      // xyla
+                        {
+                            reader.BaseStream.Position = preAdditionalPos;
+                            startTime = reader.ReadUInt32();      // start time
+                            startTickCount = reader.ReadUInt32(); // start tick count
+                            reader.BaseStream.Position = postAdditionalPos;
+                        }
+                        break;
+                    }
+                    case PktVersion.V3_1:
+                    {
+                        snifferId = reader.ReadByte();                                        // sniffer id
+                        build = reader.ReadUInt32();                                          // client build
+                        locale = Encoding.ASCII.GetString(reader.ReadBytes(4));               // client locale
+                        reader.ReadBytes(40);                                                 // session key
+                        startTime = reader.ReadUInt32();                                      // start time
+                        startTickCount = reader.ReadUInt32();                                 // start tick count
+                        additionalLength = reader.ReadInt32();
+                        var optionalData = reader.ReadBytes(additionalLength);
+                        if (snifferId == 'S') // WSTC
+                        {
+                            // versions 1.5 and older store human readable sniffer description string in header
+                            // version 1.6 adds 3 bytes before that data, 0xFF separator, one byte for major version and one byte for minor version, expecting 0x0106 for 1.6
+                            if (additionalLength >= 3 && optionalData[0] == 0xFF)
+                                snifferVersion = BitConverter.ToInt16(optionalData, 1);
+                            else
+                                snifferVersion = 0x0105;
+
+                            if (snifferVersion >= 0x0107)
+                                startTime = BitConverter.ToInt64(optionalData, 3);
+                        }
+                        break;
+                    }
+                    default:
+                    {
+                        // not supported version - let's assume the PKT bytes were a coincidence
+                        reader.BaseStream.Position = 0;
+                        break;
+                    }
+                }
+            }
+            return build;
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            if (!System.IO.File.Exists(Path.Combine(txtWppDirectory.Text, "WowPacketParser.exe")))
+            { 
+                MessageBox.Show("WowPacketParser.exe not found in directory!", "Parsing Helper", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (String.IsNullOrEmpty(txtSniffsDirectory.Text) || !Directory.Exists(txtSniffsDirectory.Text))
+            {
+                MessageBox.Show("Invalid sniff directory!", "Parsing Helper", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (cmbMode.SelectedIndex == 0)
+            {
+                foreach (ListViewItem item in listView1.Items)
+                {
+                    if (!item.Checked)
+                        continue;
+
+                    this.Text = "Parsing " + item.Text;
+
+                    List<string> fileList = (List<string>)item.Tag;
+                    CreateIndividualParseBatchFile(item.Text, fileList);
+                    System.Diagnostics.Process.Start("parse.bat").WaitForExit();
+                }
+            }
+            else if (cmbMode.SelectedIndex == 1)
+            {
+                if (String.IsNullOrEmpty(txtName.Text))
+                {
+                    MessageBox.Show("You must enter your name for mass parses!", "Parsing Helper", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                foreach (ListViewItem item in listView1.Items)
+                {
+                    if (!item.Checked)
+                        continue;
+
+                    this.Text = "Parsing " + item.Text;
+
+                    List<string> fileList = (List<string>)item.Tag;
+                    UpdateMassParseConfig(item.Text.ToLower(), txtName.Text.ToLower());
+                    CreateMassParseBatchFile(item.Text, fileList);
+                    System.Diagnostics.Process.Start("parse.bat").WaitForExit();
+                }
+            }
+            else
+            {
+                MessageBox.Show("No mode selected!", "Parsing Helper", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            MessageBox.Show("Done!", "Parsing Helper");
+        }
+
+        private void UpdateMassParseConfig(string version, string name)
+        {
+            string configText = "";
+            foreach (string line in System.IO.File.ReadLines("mass.config"))
+            {
+                if (!String.IsNullOrEmpty(configText))
+                    configText += Environment.NewLine;
+
+                string tmp = line;
+                if (tmp.Contains(MASS_PARSE_VERSION_PH))
+                    tmp = tmp.Replace(MASS_PARSE_VERSION_PH, version);
+                if (tmp.Contains(MASS_PARSE_NAME_PH))
+                    tmp = tmp.Replace(MASS_PARSE_NAME_PH, name);
+                configText += tmp;
+            }
+
+            File.WriteAllText(Path.Combine(txtWppDirectory.Text, "WowPacketParser.exe.config"), configText);
+        }
+
+        private void CreateIndividualParseBatchFile(string build, List<string> files)
+        {
+            string batchContents = "";
+            batchContents += "echo Starting individual parse of " + build + "." + Environment.NewLine;
+            batchContents += "xcopy /y \"individual.config\" \"" + Path.Combine(txtWppDirectory.Text, "WowPacketParser.exe.config") + "\"" + Environment.NewLine;
+            batchContents += Path.GetPathRoot(txtSniffsDirectory.Text).Replace("\\", "") + Environment.NewLine;
+            batchContents += "cd \"" + txtSniffsDirectory.Text + "\"" + Environment.NewLine;
+
+            string wppPath = "\"" + Path.Combine(txtWppDirectory.Text, "WowPacketParser.exe") + "\"";
+            int counter = 0;
+
+            foreach (var file in files)
+            {
+                string shortFilePath = file.Replace(txtSniffsDirectory.Text + "\\", "");
+
+                if (counter > 2)
+                {
+                    batchContents += "timeout 30" + Environment.NewLine;
+                    counter = 0;
+                }
+                else
+                {
+                    batchContents += "start \"" + Path.GetFileName(file) + "\" ";
+                    counter++;
+                }
+
+                batchContents += wppPath + " \"" + shortFilePath + "\"" + Environment.NewLine;
+            }
+
+            batchContents += "exit";
+            File.WriteAllText("parse.bat", batchContents);
+        }
+
+        private void CreateMassParseBatchFile(string build, List<string> files)
+        {
+            string batchContents = "";
+            batchContents += "echo Starting mass parse of " + build + "." + Environment.NewLine;
+            //batchContents += "xcopy /y \"mass.config\" \"" + Path.Combine(txtWppDirectory.Text, "WowPacketParser.exe.config") + "\"" + Environment.NewLine;
+            batchContents += Path.GetPathRoot(txtSniffsDirectory.Text).Replace("\\", "") + Environment.NewLine;
+            batchContents += "cd \"" + txtSniffsDirectory.Text + "\"" + Environment.NewLine;
+            batchContents += "\"" + Path.Combine(txtWppDirectory.Text, "WowPacketParser.exe") + "\"";
+            foreach (var file in files)
+            {
+                string shortFilePath = file.Replace(txtSniffsDirectory.Text + "\\", "");
+                batchContents += " \"" + shortFilePath + "\"";
+            }
+            batchContents += Environment.NewLine + "exit";
+            File.WriteAllText("parse.bat", batchContents);
+        }
+
+        private void btnReverseSelection_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listView1.Items)
+            {
+                item.Checked = !item.Checked;
+            }
+        }
+    }
+}
